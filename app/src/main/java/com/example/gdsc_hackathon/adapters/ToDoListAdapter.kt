b4bfe4aa41342e5b69dd8686cd6bee0fe9d05b0b
Tejas <tvborkar1234@gmail.com>
2021-12-26 11:43:30 +0530

    updating and deleting feature for todo list

--- app/src/main/java/com/example/gdsc_hackathon/adapters/ToDoListAdapter.kt
+++ app/src/main/java/com/example/gdsc_hackathon/adapters/ToDoListAdapter.kt
@@ -2,50 +2,98 @@ package com.example.gdsc_hackathon.adapters
 
 
 import android.app.Activity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
+import android.content.Intent
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gdsc_hackathon.activities.AddToDoItem
 import com.example.gdsc_hackathon.dataModel.ToDoModel
 import com.example.gdsc_hackathon.databinding.SingleToDoItemBinding
+import com.google.android.material.snackbar.Snackbar
+import java.io.File
 
 
 class ToDoListAdapter(
- private val activity: Activity,
-    private val listItems: List<ToDoModel>,
+    private val activity: Activity,
 
-) : RecyclerView.Adapter<ToDoListAdapter.ViewHolder>() {
+    ) : RecyclerView.Adapter<ToDoListAdapter.ViewHolder>() {
     class ViewHolder(view: SingleToDoItemBinding) : RecyclerView.ViewHolder(view.root) {
         val titleText = view.todoListTitle
         val description  = view.todoListDescription
+        val todoItem = view.todoListSingleItem
 
     }
+    private var removedPosition: Int = 0
+    private var listItems = getToDoList()
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): ToDoListAdapter.ViewHolder {
+    ): ViewHolder {
         val binding: SingleToDoItemBinding = SingleToDoItemBinding
             .inflate(LayoutInflater.from(activity), parent, false)
+
         return ViewHolder(binding)
     }
 
-
-    override fun onBindViewHolder(holder: ToDoListAdapter.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = listItems[position]
         holder.titleText.text = item.title
         holder.description.text = item.description
-//        holder.tvText.setOnClickListener{
-//            if(activity is AddDish){
-//                activity.selectedListItem(item,selection)
-//            }
-//        }
+        holder.todoItem.setOnClickListener{
+            val intent = Intent(activity, AddToDoItem::class.java)
+            intent.putExtra(AddToDoItem.TITLE,item.title)
+            intent.putExtra(AddToDoItem.DESCRIPTION,item.description)
+            activity.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
         return listItems.size
+    }
+    fun removeItem(position: Int, viewHolder: RecyclerView.ViewHolder) {
+        val deletedTodo :ToDoModel= listItems[position]
+        removedPosition = position
+        val directory = File("${activity.filesDir?.absolutePath?.toString()}/${AddToDoItem.FILE_DIRECTORY}")
+        listItems.removeAt(position)
+        File(directory, "${deletedTodo.title}.txt").delete()
+        listItems =  getToDoList()
+        notifyItemRemoved(position)
+        notifyItemRangeChanged(position, listItems.size)
+        Snackbar.make(viewHolder.itemView, "${deletedTodo.title} removed", Snackbar.LENGTH_LONG).setAction("UNDO") {
+            listItems.add(removedPosition, deletedTodo)
+            val file = File(directory, "${deletedTodo.title}.txt")
+            file.appendText(deletedTodo.description)
+            notifyItemInserted(removedPosition)
+
+        }.show()
+    }
+    private fun getToDoList() : ArrayList<ToDoModel>{
+        val toDoList = ArrayList<ToDoModel>()
+        val path = activity.filesDir?.absolutePath?.toString()
+        if (path==null) {
+            Toast.makeText(activity, "Files are not getting fetch", Toast.LENGTH_LONG).show()
+
+        }
+        else {
+
+            val directory = File("$path/${AddToDoItem.FILE_DIRECTORY}")
+            if(!directory.exists()){
+                directory.mkdir()
+            }
+            val files  = directory.listFiles()
+            if(files!=null)
+                for(file in files){
+                    val title = file.name.split(".txt")[0]
+                    val description = file.readText()
+
+                    val todo = ToDoModel(title, description)
+                    toDoList.add(todo)
+                }
+
+        }
+        return toDoList
     }
 }
--- app/src/main/java/com/example/gdsc_hackathon/fragments/TodoListFragment.kt
+++ app/src/main/java/com/example/gdsc_hackathon/fragments/TodoListFragment.kt
@@ -1,27 +1,30 @@
 package com.example.gdsc_hackathon.fragments
 
+
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.gdsc_hackathon.R
+import androidx.recyclerview.widget.RecyclerView
+
+
 import com.example.gdsc_hackathon.activities.AddToDoItem
+
 import com.example.gdsc_hackathon.adapters.ToDoListAdapter
-import com.example.gdsc_hackathon.dataModel.ToDoModel
+
 import com.example.gdsc_hackathon.databinding.FragmentTodoListBinding
-import java.io.BufferedReader
-import java.io.File
-import java.io.FileInputStream
-import java.io.InputStreamReader
+
 
 
 class TodoListFragment : Fragment(), View.OnClickListener {
     private lateinit var binding: FragmentTodoListBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,55 +32,38 @@ class TodoListFragment : Fragment(), View.OnClickListener {
         binding = FragmentTodoListBinding.inflate(inflater, container, false)
         binding.floatingActionButtonToAddItemList.setOnClickListener(this)
         getTodoListData()
-
-
-
         return binding.root
 
     }
 
     private fun getTodoListData() {
         binding.recycleViewForTodoList.layoutManager = LinearLayoutManager(activity)
-        val path = activity?.filesDir?.absolutePath?.toString()
-        if (path == null) {
-            Toast.makeText(activity, "Files are not getting fetch", Toast.LENGTH_LONG).show()
-        }
-        else {
-            val toDoList = mutableListOf<ToDoModel>()
-            var fileInputStream: FileInputStream?
-            val stringBuilder: StringBuilder = StringBuilder()
-            var text: String?
-            Log.d("Files", "Path: $path")
-            val directory = File(path)
-            Log.d("Files", "Path: $path")
-            val files = directory.listFiles()
-            Log.d("Files", "Size: " + files?.size)
-            if (files != null)
-                for (i in files.indices) {
-                    fileInputStream = context?.openFileInput(files[i].name)
-                    val inputStreamReader = InputStreamReader(fileInputStream)
-                    val bufferedReader = BufferedReader(inputStreamReader)
-                    while (run {
-                            text = bufferedReader.readLine()
-                            text
-                        } != null) {
-                        stringBuilder.append(text)
-                    }
-                    val todo = ToDoModel(files[i].name, stringBuilder.toString())
-                    toDoList.add(todo)
-                    Log.d("Files", "FileName:" + files[i].name)
-                }
-            val adapter = activity?.let { ToDoListAdapter(it, toDoList) }
-            binding.recycleViewForTodoList.adapter = adapter
 
-        }
+        val adapter = activity?.let { ToDoListAdapter(it) }
+        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+
+                adapter?.removeItem( viewHolder.adapterPosition,viewHolder)
+
+            }
+        }).attachToRecyclerView(binding.recycleViewForTodoList)
+        binding.recycleViewForTodoList.adapter = adapter
+        binding.recycleViewForTodoList.invalidate()
 
     }
 
     override fun onClick(p0: View?) {
 
         when (p0?.id) {
-            R.id.floating_action_button_to_add_item_list -> {
+            com.example.gdsc_hackathon.R.id.floating_action_button_to_add_item_list -> {
 
                 activity?.let {
                     val intent = Intent(it, AddToDoItem::class.java)
@@ -86,13 +72,10 @@ class TodoListFragment : Fragment(), View.OnClickListener {
 
             }
         }
-
-
     }
 
     override fun onResume() {
         super.onResume()
         getTodoListData()
     }
-
 }
