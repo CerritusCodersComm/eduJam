9ff3b1183fdf6a053bd0b94f8fa320f42f9727b0
cdhiraj40 <chauhandhiraj40@gmail.com>
2022-01-07 12:48:46 +0530

    app done

---
+++ app/src/main/java/com/example/gdsc_hackathon/activities/SplashScreenActivity.kt
@@ -0,0 +1,50 @@
+package com.example.gdsc_hackathon.activities
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import com.example.gdsc_hackathon.R
+import com.example.gdsc_hackathon.databinding.ActivitySplashScreenBinding
+import com.example.gdsc_hackathon.onboard.OnBoardActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.setFlags(WindowManager.LayoutParams.FIRST_SYSTEM_WINDOW, WindowManager.LayoutParams.FIRST_SYSTEM_WINDOW)
+        val topAnimation= AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        val middleAnimation= AnimationUtils.loadAnimation(this, R.anim.middle_animation)
+        val first=binding.green
+        val second=binding.blue
+        val third=binding.greay
+        val fourth=binding.red
+        val fifth=binding.blue1
+        val sixth=binding.green1
+        val seventh=binding.greay1
+        val title=binding.title
+        first.animation = topAnimation
+        second.animation = topAnimation
+        third.animation = topAnimation
+        fourth.animation = topAnimation
+        fifth.animation = topAnimation
+        sixth.animation = topAnimation
+        seventh.animation = topAnimation
+        title.animation=middleAnimation
+        Handler(Looper.getMainLooper()).postDelayed({
+            val startAct= Intent(this, OnBoardActivity::class.java)
+            startActivity(startAct)
+            finish()
+        }, 2000)
+
+
+    }
+}
